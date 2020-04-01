e78e9331da975b5fa0127d7da94a2a792d32648f
Andrea <andreademarco91@gmail.com>
2020-04-01 17:16:37 +0200

    UPUP CONTROL

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,12 @@
     <div class="box">
       <h2><a href="{{route('show', $post->id)}}">{{$post['title']}}</a></h2> <br>
         <span><a href="{{route('show', $post->id)}}">{{$post->user->name}}</a></span>
-      <p>Corpo: {{$post['body']}}</p>
+      <p>{{$post['body']}}</p>
       <img src="{{$post['photo_path']}}" alt="">
-      <h6>Tags:
-        <p>
-          @foreach ($post->tags as $tag)
-          {{$tag->tag}}
+      <h6>{{($post->tags->isEmpty()) ? '' : 'HANNO COLLABORATO:'}}     </h6>
+        @foreach ($post->tags as $tag)
+          <p>{{$tag->tag}}</p>
         @endforeach
-        </p>
-      </h6>
       <a href="{{route('show', $post->id)}}">Guarda e commenta </a>
     </div>
   @endforeach
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,13 +5,16 @@
       <h2>{{$post['title']}} di {{$post->user->name}}</h2>
       <p>Corpo: {{$post['body']}}</p>
       <img src="{{$post['photo_path']}}" alt="">
-      <h6>Tags: @foreach ($post->tags as $tag)<p>{{$tag->tag}}</p> </h6> @endforeach
-      <h6>Commenti:</h6>
-        @foreach ($post->comment as $comment)
-      <ul class="comment">
-        <li>{{$comment['text']}} - <small>{{$comment['created_at']}}</small></li>
-      </ul>
+      <h6>{{($post->tags->isEmpty()) ? '' : 'HANNO COLLABORATO:'}}     </h6>
+        @foreach ($post->tags as $tag)
+          <p>{{$tag->tag}}</p>
         @endforeach
+      <ul class="comment">
+        <h6>Commenti:</h6>
+        @foreach ($post->comment as $comment)
+          <li>{{$comment['text']}} - <small>{{$comment['created_at']}}</small></li>
+        @endforeach
+        </ul>
       <form class="" action="{{route('comment', $post->id)}}" method="post">
         <input name="text" rows="8" cols="80"></input>
         <button class="btn btn-primary"type="submit" name="button">Commenta</button>
